76b991d5acae4ae9990b9fbd2b9bc9b8ce2482ac
Nkamenyin <valentineeyet01@gmail.com>
2024-01-06 14:35:24 +0100

    ifloop

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+last_dgit = number % 10
+
+print("Last digit of", number, "is", last_dgit)
+
+if last_dgit > 5:
+    print(f"and is greater than 5")
+elif last_dgit  == 0:
+    print(f"and is 0")
+else:
+    print(f"and is less than 6 and not 0")
