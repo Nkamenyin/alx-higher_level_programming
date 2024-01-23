c5504231671011940af732d665fa202b103255be
Nkamenyin <valentineeyet01@gmail.com>
2024-01-23 22:52:55 +0100

    clss

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""class is called Suare."""
+
+
+class Square:
+    """Defines a Square"""
+
+    def __init__(self, size=0):
+        """constructor
+
+        Args:
+            size: size is the lenght of sideof Square"""
+
+        self.__size = size
